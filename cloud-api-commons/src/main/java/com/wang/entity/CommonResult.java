9e94fc619288e83425d8f3177d0cbccd19cc3df2
wangzhe <2234844362@qq.com>
2021-07-23 20:03:09 +0800

    实体类，工具类单独提出来作为一个模块

---
+++ cloud-api-commons/src/main/java/com/wang/entity/CommonResult.java
@@ -0,0 +1,24 @@
+package com.wang.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Auther Wang zhe
+ * @Version
+ * @ClassName
+ * @Description
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommonResult <T>{
+    private Integer code;
+    private String message;
+    private T data;
+
+    public CommonResult(Integer code,String message){
+        this(code,message,null);
+    }
+}
--- cloud-payment8001/src/main/java/com/wang/controller/PaymentController.java
+++ cloud-payment8001/src/main/java/com/wang/controller/PaymentController.java
@@ -1,6 +1,6 @@
 package com.wang.controller;
 
-import com.wang.entity.CommenResult;
+import com.wang.entity.CommonResult;
 import com.wang.entity.Payment;
 import com.wang.service.IPaymentService;
 import lombok.extern.slf4j.Slf4j;
@@ -22,25 +22,25 @@ public class PaymentController {
     private IPaymentService paymentService;
 
     @PostMapping(value = "/payment/create")
-    public CommenResult create(@RequestBody Payment payment){
+    public CommonResult create(@RequestBody Payment payment){
         int result = paymentService.create(payment);
         if (result>0){
             log.info("插入成功:{}！，参数：{}",result,payment.getSerial());
-            return new CommenResult(200,"插入成功",result);
+            return new CommonResult(200,"插入成功",result);
         }else {
             log.info("插入失败！",result);
-            return new CommenResult(444,"插入失败!");
+            return new CommonResult(444,"插入失败!");
         }
     }
 
     @GetMapping(value = "/payment/get/{id}")
-    public CommenResult selectById(@PathVariable("id") Integer id){
+    public CommonResult selectById(@PathVariable("id") Integer id){
         Payment payment = paymentService.getPaymentById(id);
         if (null!=payment){
             log.info("插入成功！");
-            return new CommenResult(200,"查询成功",payment);
+            return new CommonResult(200,"查询成功",payment);
         }else {
-            return new CommenResult(444,"没有找到ID:"+id);
+            return new CommonResult(444,"没有找到ID:"+id);
         }
     }
 }
